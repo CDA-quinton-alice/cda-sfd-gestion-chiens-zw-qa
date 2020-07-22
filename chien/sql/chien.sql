ceb89d3360f66fa288e39d2f70e069d53dbdd328
Alice Quinton <quinton.alice@gmail.com>
2020-07-22 12:41:28 +0200

    changement des id underscore en camelCase sur les scripts
    changement des id underscore en camelCase sur les scripts sql

--- chien/sql/chien.sql
+++ chien/sql/chien.sql
@@ -23,12 +23,12 @@ FLUSH PRIVILEGES;
 #------------------------------------------------------------
 
 create table personne(
-	'id_personne' int primary key auto_increment,
-	'login varchar'(20) not null unique,
-	'mdp' varchar(32) not null,
-	'email' varchar(200) not null
+	idPersonne int primary key auto_increment,
+	login varchar(20) not null unique,
+	mdp varchar(32) not null,
+	email varchar(200) not null
 ) ENGINE = InnoDB;
-insert into personne(`login`,`password`,`email`) values('admin','admin','admin@gmail.com');
+insert into personne(`login`,`mdp`,`email`) values('admin','admin','admin@gmail.com');
 select * from personne;
 
 #------------------------------------------------------------
@@ -37,20 +37,20 @@ select * from personne;
 
 
 create table chien(
-'id_chien' int primary key auto_increment,	
-	'puce' int not null,
-	'nom' varchar(100) not null,
-	'couleur' varchar(100) not null,
-	'age' int not null,
-	'img_path' varchar(200)
+idChien int primary key auto_increment,	
+	puce int not null,
+	nom varchar(100) not null,
+	couleur varchar(100) not null,
+	age int not null,
+	imgPath varchar(200)
 )	ENGINE = InnoDB;
 
 #------------------------------------------------------------
 #        Création de la table personne_chien
 #------------------------------------------------------------
 create table personne_chien(
-	'id_personne' INT,
-   'id_chien' INT,
+	id_personne INT,
+   id_chien INT,
    PRIMARY KEY(id_personne, id_chien),
    FOREIGN KEY(id_personne) REFERENCES Personne(id_personne),
    FOREIGN KEY(id_chien) REFERENCES Chien(id_chien)
