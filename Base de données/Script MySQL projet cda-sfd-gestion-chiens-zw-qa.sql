8f49b7915dff7af3180348d6cd5c92188282928a
Alice Quinton <quinton.alice@gmail.com>
2020-07-22 11:53:23 +0200

    Revert "Merge branch 'dev' into Alice"
    This reverts commit f745483958e0765cfee217a1c27f22dbc325cdcc.

--- Base de données/Script MySQL projet cda-sfd-gestion-chiens-zw-qa.sql
+++ Base de données/Script MySQL projet cda-sfd-gestion-chiens-zw-qa.sql
@@ -17,16 +17,15 @@ use gestion_chiens;
 CREATE USER if not EXISTS'user_gestion_chiens'@'localhost' IDENTIFIED BY 'pwd';
 grant all privileges on gestion_chiens.* to 'user_gestion_chiens'@'localhost';
 FLUSH PRIVILEGES;
-
 #------------------------------------------------------------
 #        Création de la table personne 
 #------------------------------------------------------------
 
 create table personne(
-	'id_personne' int primary key auto_increment,
-	'login varchar'(20) not null unique,
-	'mdp' varchar(32) not null,
-	'email' varchar(200) not null
+	`id` int primary key auto_increment,
+	`login` varchar(20) not null unique,
+	`password` varchar(32) not null,
+	`email` varchar(200)
 ) ENGINE = InnoDB;
 insert into personne(`login`,`password`,`email`) values('admin','admin','admin@gmail.com');
 select * from personne;
@@ -37,21 +36,10 @@ select * from personne;
 
 
 create table chien(
-'id_chien' int primary key auto_increment,	
-	'puce' int not null,
-	'nom' varchar(100) not null,
-	'couleur' varchar(100) not null,
-	'age' int not null,
-	'img_path' varchar(200)
+	`id` int primary key auto_increment,	
+	`puce` int not null,
+	`nom` varchar(100) not null,
+	`couleur` varchar(100) not null,
+	`age` int not null,
+	`img_path` varchar(200) not null
 )	ENGINE = InnoDB;
-
-#------------------------------------------------------------
-#        Création de la table personne_chien
-#------------------------------------------------------------
-create table personne_chien(
-	'id_personne' INT,
-   'id_chien' INT,
-   PRIMARY KEY(id_personne, id_chien),
-   FOREIGN KEY(id_personne) REFERENCES Personne(id_personne),
-   FOREIGN KEY(id_chien) REFERENCES Chien(id_chien)
-)ENGINE=InnoDB;
