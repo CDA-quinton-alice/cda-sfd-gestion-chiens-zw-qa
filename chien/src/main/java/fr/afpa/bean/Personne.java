e5c910ca08930d54d789efc1a02cef12425cc609
Alice Quinton <quinton.alice@gmail.com>
2020-07-22 16:13:15 +0200

    rajout de toString dans Personne
    rajout de toString dans Personne

--- chien/src/main/java/fr/afpa/bean/Personne.java
+++ chien/src/main/java/fr/afpa/bean/Personne.java
@@ -3,10 +3,12 @@ package fr.afpa.bean;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Personne {
 
 	private int idPersonne;
